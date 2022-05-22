4398f994631283c74b7a1302c23602e5d805c56b
ELYOUSFI Mohamed <mohamed5elyousfi@gmail.com>
2022-05-22 02:28:56 +0100

    ✅ export result as pdf done

--- Frontend/M-Orientation_Frontend/src/app/component/result/result.component.ts
+++ Frontend/M-Orientation_Frontend/src/app/component/result/result.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild,ElementRef } from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
 import {Output} from "../../model/output";
+import {jsPDF} from "jspdf";
 
 @Component({
   selector: 'app-result',
@@ -8,11 +9,21 @@ import {Output} from "../../model/output";
   styleUrls: ['./result.component.css']
 })
 export class ResultComponent implements OnInit {
+  @ViewChild('saved' ,{static : false}) el! : ElementRef;
 
   result: Output = {'resultat' : ""};
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => this.result.resultat = params['resultat']);
+  }
+
+  genererPDF() {
+    let pdf =  new jsPDF('l','pt','tabloid');
+    pdf.html(this.el.nativeElement,{
+      callback: (pdf)=> {
+        pdf.save("result.pdf");
+      }
+    })
   }
 }
